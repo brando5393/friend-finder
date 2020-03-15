f092171e7b67aff432191a1264992be4cdbdb6b9
Brandon Williams <brando5393@gmail.com>
2020-03-15 16:15:44 -0500

    not a function error problem in server.js

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,6 @@
+exports = (app) => {
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname + '../public/home.html')));
+
+    app.get('/survey', (req, res) => res.sendFile(path.join(__dirname + '../public/survey.html')));
+
+};
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.Port || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+app.listen(port,() => console.log(`Server started on localhost:${port}`));
